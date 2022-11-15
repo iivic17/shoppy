8855e8bec8d7c3d05492a3a55e91186897cf9f6b
iivic17 <ilijaivic17@gmail.com>
2022-11-15 23:53:53 +0100

    Fixed settings button created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import './App.css';
 
 const App = () => {
-	return <h1 className='underline text-3xl'>App</h1>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div className='flex relative dark:bg-main-dark-bg'>
+					<div className='fixed right-5 bottom-5 z-[1000]'>
+						<TooltipComponent content='Settings' position='TopCenter'>
+							<button
+								type='button'
+								className={`text-3xl p-3 hover:drop-shadow-xl text-white bg-[blue] rounded-full`}>
+								<FiSettings />
+							</button>
+						</TooltipComponent>
+					</div>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
