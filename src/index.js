7a859cf04383aabaca678d0697a8cbb799b00c7d
iivic17 <ilijaivic17@gmail.com>
2022-11-17 00:06:35 +0100

    Active menu connected to view

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
 import './index.css';
 
 const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
+root.render(
+	<ContextProvider>
+		<App />
+	</ContextProvider>
+);
