a9300e0299ed7ef270b2a0917b99ec12b1b6fca1
iivic17 <ilijaivic17@gmail.com>
2022-11-18 01:13:03 +0100

    Button component created

--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -1,5 +1,34 @@
-const ECommerce = () => {
-	return <div>ECommerce</div>;
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { StackedChart, PieChart, Button, SparkLineChart } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
+
+import { earningData, sparklineAreaData, ecomPieChartData } from '../data/dummy';
+
+const Ecommerce = () => {
+	return (
+		<div className='mt-12'>
+			<div className='flex flex:wrap lg:flex-nowrap justify-center'>
+				<div className='bg-white dark:gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
+					<div className='flex justify-between items-center'>
+						<div>
+							<p className='font-bold text-gray-400'>Earnings</p>
+							<p className='text-2xl'>$63,448.78</p>
+						</div>
+					</div>
+					<div className='mt-6'>
+						<Button
+							color='white'
+							bgColor='blue'
+							text='Download'
+							borderRadius='10px'
+							size='md'
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-export default ECommerce;
+export default Ecommerce;
