609de76ef0dbb1184288893fec73e494fabe4c4a
iivic17 <ilijaivic17@gmail.com>
2022-11-18 18:32:51 +0100

    Ecommerce page ui issue fixed

--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -8,7 +8,7 @@ import { earningData, sparklineAreaData, ecomPieChartData } from '../data/dummy'
 const Ecommerce = () => {
 	return (
 		<div className='mt-24 max-w-7xl mx-auto'>
-			<div className='flex justify-center flex-col xl:flex-row'>
+			<div id='first-row' className='flex justify-center flex-col xl:flex-row'>
 				<div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl flex-1 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
 					<div className='flex justify-between items-center'>
 						<div>
@@ -55,7 +55,9 @@ const Ecommerce = () => {
 				</div>
 			</div>
 
-			<div className='flex justify-center flex-col xl:flex-row'>
+			<div
+				id='second-row'
+				className='flex justify-center flex-col xl:flex-row items-stretch'>
 				<div
 					id='revenue'
 					className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl w-full'>
