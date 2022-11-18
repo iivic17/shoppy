aa9ff0c0e26ae89a62649c7218539cd79af89e3e
iivic17 <ilijaivic17@gmail.com>
2022-11-18 17:19:18 +0100

    Sparkline chart component created

--- src/components/Charts/SparkLineChart.jsx
+++ src/components/Charts/SparkLineChart.jsx
@@ -1,5 +1,34 @@
-const SparkLineChart = () => {
-	return <div>SparkLineChart</div>;
+import {
+	SparklineComponent,
+	Inject,
+	SparklineTooltip,
+} from '@syncfusion/ej2-react-charts';
+
+const SparkLineChart = ({ id, height, width, color, currentColor, type, data }) => {
+	return (
+		<SparklineComponent
+			id={id}
+			height={height}
+			width={width}
+			lineWidth={1}
+			valueType='Numeric'
+			fill={color}
+			border={{ color: currentColor, width: 2 }}
+			tooltipSettings={{
+				visible: true,
+				format: '${x} : data ${yval}',
+				trackLineSettings: {
+					visible: true,
+				},
+			}}
+			markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+			dataSource={data}
+			xName='x'
+			yName='yval'
+			type={type}>
+			<Inject services={[SparklineTooltip]} />
+		</SparklineComponent>
+	);
 };
 
 export default SparkLineChart;
