225cbcee18903e1006578a92c9b68fe60a3ba55e
iivic17 <ilijaivic17@gmail.com>
2022-11-19 00:11:18 +0100

    Employees page created

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,5 +1,38 @@
+import {
+	GridComponent,
+	ColumnsDirective,
+	ColumnDirective,
+	Page,
+	Edit,
+	Inject,
+	Search,
+	Toolbar,
+} from '@syncfusion/ej2-react-grids';
+import { Header } from '../components';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+
 const Employees = () => {
-	return <div>Employees</div>;
+	return (
+		<div className='my-10 mx-3 sm:mx-10 p-5 sm:p-10 pt-10 bg-white rounded-3xl'>
+			<Header title='Employees' category='Page' />
+			<GridComponent
+				dataSource={employeesData}
+				allowPaging
+				pageSettings={{
+					pageSize: 20,
+				}}
+				toolbar={['Search']}
+				width='auto'>
+				<ColumnsDirective>
+					{employeesGrid.map(item => (
+						<ColumnDirective key={item.headerText} {...item} />
+					))}
+				</ColumnsDirective>
+				<Inject services={[Page, Edit, Search, Toolbar]} />
+			</GridComponent>
+		</div>
+	);
 };
 
 export default Employees;
