5b6844b29932c8ec652f7ec22a86837404567169
iivic17 <ilijaivic17@gmail.com>
2022-11-19 14:19:44 +0100

    Page template spacing adjusted

--- src/pages/PageTemplate.jsx
+++ src/pages/PageTemplate.jsx
@@ -6,7 +6,9 @@ const PageTemplate = ({ title, category, children }) => {
 			id={`template-${title}`}
 			className='my-10 mx-3 sm:mx-10 p-5 sm:p-10 pt-10 bg-white rounded-3xl'>
 			<Header title={title} category={category} />
-			{children}
+			<div id={`page-${title}`} className='mt-10'>
+				{children}
+			</div>
 		</div>
 	);
 };
