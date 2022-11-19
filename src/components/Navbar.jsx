c74f706b30a1c95d81f4c5b4a44c5fdac3b2ddfd
iivic17 <ilijaivic17@gmail.com>
2022-11-19 21:56:44 +0100

    Navbar responsiveness and ui updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,10 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 		<button
 			type='button'
 			onClick={customFunc}
-			className='relative text-xl rounded-full p-3 hover:bg-light-gray'
+			className='flex items-center justify-center relative text-xl rounded-full p-3 hover:bg-light-gray w-10 h-10 sm:w-16 sm:h-16'
 			style={{ color: color }}>
 			<span
-				className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
+				className='absolute flex align-center justify-center rounded-full h-2 w-2 right-2 top-2'
 				style={{ backgroundColor: dotColor }}
 			/>
 			{icon}
@@ -67,44 +67,44 @@ const Navbar = () => {
 				title='Menu'
 				customFunc={() => setActiveMenu(prev => !prev)}
 				color={currentColor}
-				icon={<AiOutlineMenu />}
+				icon={<AiOutlineMenu className='w-4 sm:w-6 h-4 sm:h-6' />}
 			/>
 			<div className='flex flex-row justify-between items-center gap-2'>
 				<NavButton
 					title='Cart'
 					customFunc={() => handleClick('cart')}
 					color={currentColor}
-					icon={<FiShoppingCart />}
+					icon={<FiShoppingCart className='w-4 sm:w-6 h-4 sm:h-6' />}
 				/>
 				<NavButton
 					title='Chat'
 					dotColor='#03C9D7'
 					customFunc={() => handleClick('chat')}
 					color={currentColor}
-					icon={<BsChatLeft />}
+					icon={<BsChatLeft className='w-4 sm:w-6 h-4 sm:h-6' />}
 				/>
 				<NavButton
 					title='Notifications'
 					dotColor='#03C9D7'
 					customFunc={() => handleClick('notification')}
 					color={currentColor}
-					icon={<RiNotification3Line />}
+					icon={<RiNotification3Line className='w-4 sm:w-6 h-4 sm:h-6' />}
 				/>
 				<TooltipComponent content='Profile' position='BottomCenter'>
 					<div
-						className='flex items-center gap-2 cursor-pointer ml-6 p-1 hover:bg-light-gray rounded-lg'
+						className='flex items-center gap-2 cursor-pointer ml-3 sm:ml-6 p-1 hover:bg-light-gray rounded-lg mr-4 sm:mr-0'
 						onClick={() => handleClick('userProfile')}>
 						<img
 							src={avatar}
 							className='rounded-full w-10 h-10 object-contain'
 						/>
-						<p>
-							<span className='text-gray-400 text-14'>Hi,</span>{' '}
-							<span className='text-gray-400 font-bold ml-1 text-14'>
+						<p className='hidden sm:block'>
+							<span className='text-gray-400 text-lg'>Hi,</span>{' '}
+							<span className='text-gray-400 font-semibold ml-1 text-lg tracking-wider'>
 								Michael
 							</span>
 						</p>
-						<MdKeyboardArrowDown className='text-gray-400 text-14' />
+						<MdKeyboardArrowDown className='text-gray-400 text-16' />
 					</div>
 				</TooltipComponent>
 
