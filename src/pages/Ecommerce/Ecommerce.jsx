25ec298dae631b705a53c46df53b04327c54162e
iivic17 <ilijaivic17@gmail.com>
2022-11-20 17:45:57 +0100

    Weekly stats card created

--- src/pages/Ecommerce/Ecommerce.jsx
+++ src/pages/Ecommerce/Ecommerce.jsx
@@ -4,6 +4,7 @@ import FeatureBoxGroup from './FeatureBoxGroup';
 import RecentTransactionsCard from './RecentTransactionsCard';
 import RevenueUpdatesCard from './RevenueUpdatesCard';
 import SalesOverviewCard from './SalesOverviewCard';
+import WeeklyStatsCard from './WeeklyStatsCard';
 
 const Ecommerce = () => {
 	return (
@@ -27,6 +28,12 @@ const Ecommerce = () => {
 				className='mt-5 w-full flex justify-center flex-col xl:flex-row items-stretch gap-6 sm:gap-3'>
 				<RecentTransactionsCard />
 				<SalesOverviewCard />
+			</div>
+
+			<div
+				id='ecommerce-fourth-row'
+				className='mt-5 w-full flex justify-center flex-col xl:flex-row items-stretch gap-6 sm:gap-3'>
+				<WeeklyStatsCard />
 			</div>
 		</div>
 	);
---
+++ src/pages/Ecommerce/WeeklyStatsCard.jsx
@@ -0,0 +1,54 @@
+import { SparkLineChart } from '../../components';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { IoIosMore } from 'react-icons/io';
+
+import { weeklyStats, sparklineAreaData } from '../../data/dummy';
+
+const WeeklyStatsCard = () => {
+	const { currentColor } = useStateContext();
+
+	return (
+		<div className='md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 pb-8 m-3 flex-1'>
+			<div className='flex justify-between'>
+				<p className='text-xl font-semibold'>Weekly Stats</p>
+				<button type='button' className='text-xl font-semibold text-gray-500'>
+					<IoIosMore />
+				</button>
+			</div>
+
+			<div className='mt-10 '>
+				{weeklyStats.map(item => (
+					<div key={item.title} className='flex justify-between mt-4 w-full'>
+						<div className='flex gap-4'>
+							<button
+								type='button'
+								style={{ background: item.iconBg }}
+								className='text-2xl hover:drop-shadow-xl text-white rounded-full p-3'>
+								{item.icon}
+							</button>
+							<div>
+								<p className='text-md font-semibold'>{item.title}</p>
+								<p className='text-sm text-gray-400'>{item.desc}</p>
+							</div>
+						</div>
+
+						<p className={`text-${item.pcColor}`}>{item.amount}</p>
+					</div>
+				))}
+				<div className='mt-4'>
+					<SparkLineChart
+						currentColor={currentColor}
+						id='area-sparkLine'
+						height='160px'
+						type='Area'
+						data={sparklineAreaData}
+						width='auto'
+						color='rgb(242, 252, 253, .3)'
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default WeeklyStatsCard;
